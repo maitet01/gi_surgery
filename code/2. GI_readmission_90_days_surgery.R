01d4af43313829d9ac5024025123347ec44780c5
maitet01 <maite.thrower2@phs.scot>
2025-08-14 15:04:53 +0000

    Readmission for GI patients

---
+++ code/2. GI_readmission_90_days_surgery.R
@@ -0,0 +1,7 @@
+# ─────────────────────────────────────────────────────────────
+# Title: GI surgery patients readmitted within 90 days for surgical complications
+# Author: Maite de Haro Moro
+# Date: 2025-08-14
+# Description: This script loads SMRs data and filters for GI surgeries
+# ─────────────────────────────────────────────────────────────
+
