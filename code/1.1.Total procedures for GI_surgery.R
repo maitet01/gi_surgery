a4d7b883fd29ea454ceedbcc65afae820c2381a2
maitet01 <maite.thrower2@phs.scot>
2025-08-18 12:08:56 +0000

    New code for the total number of GI procedures

---
+++ code/1.1.Total procedures for GI_surgery.R
@@ -0,0 +1,7 @@
+# ─────────────────────────────────────────────────────────────
+# Title: Extract Gastrointestinal Surgeries from SMRs Data
+# Author: Maite de Haro Moro
+# Date: 2025-08-13
+# Description: This script produces the total number of procedures for GI surgeries
+# ─────────────────────────────────────────────────────────────
+
