3775f4597e2b387f9833461dd03f6916b582cbbf
maitet01 <maite.thrower2@phs.scot>
2025-08-18 12:12:11 +0000

    change date

--- code/1.1.Total procedures for GI_surgery.R
+++ code/1.1.Total procedures for GI_surgery.R
@@ -1,7 +1,7 @@
 # ─────────────────────────────────────────────────────────────
 # Title: Extract Gastrointestinal Surgeries from SMRs Data
 # Author: Maite de Haro Moro
-# Date: 2025-08-13
+# Date: 2025-08-18
 # Description: This script produces the total number of procedures for GI surgeries
 # ─────────────────────────────────────────────────────────────
 
